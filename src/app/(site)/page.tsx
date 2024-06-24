5c2792717d56b3dff2a31775019a25f08a212440
imranDev20 <imrandev20@gmail.com>
2024-06-24 10:39:38 +0600

    Add lazyloading inside slider

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,23 +1,23 @@
 import Hero from "./_components/hero";
-// import ServiceCategories from "./_components/service-categories";
-// import Testimonials from "./_components/testimonials/testimonials";
+import ServiceCategories from "./_components/service-categories";
+import Testimonials from "./_components/testimonials/testimonials";
 import dynamic from "next/dynamic";
 
-const ServiceCategories = dynamic(
-  () => import("./_components/service-categories"),
-  {
-    loading: () => <p>Loading...</p>,
-    ssr: true, // Set to true if you want server-side rendering
-  }
-);
+// const ServiceCategories = dynamic(
+//   () => import("./_components/service-categories"),
+//   {
+//     loading: () => <p>Loading...</p>,
+//     ssr: true, // Set to true if you want server-side rendering
+//   }
+// );
 
-const Testimonials = dynamic(
-  () => import("./_components/testimonials/testimonials"),
-  {
-    loading: () => <p>Loading...</p>,
-    ssr: true, // Set to true if you want server-side rendering
-  }
-);
+// const Testimonials = dynamic(
+//   () => import("./_components/testimonials/testimonials"),
+//   {
+//     loading: () => <p>Loading...</p>,
+//     ssr: true, // Set to true if you want server-side rendering
+//   }
+// );
 
 export default function Home() {
   return (
